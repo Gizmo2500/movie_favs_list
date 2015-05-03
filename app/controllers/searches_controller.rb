bf659fc4adc9b6ccdd2bedb3ee98af46d3315741
David Spencer <davidspencer@Davids-MacBook-Pro-4.local>
2015-05-03 16:16:15 -0700

    add omdb searchengine

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,22 @@
 class SearchesController < ApplicationController
   def index
-  
+  	if params[:q]
+  	 resp = Typhoeus.get("http://omdbapi.com", :params => {:s => params[:q]})
+  	 json_resp = JSON.parse(resp.body)
+  	 @search = json_resp["Search"]
+  	else
+  	@search = []
+  	end
   end
 
   def movie
+  	if params[:q]
+  	resp = Typhoeus.get("http://omdbapi.com", :params => {:i => params[:q]})
+  	json_resp = JSON.parse(resp.body)
+  	@movie = json_resp
+  	else
+  	 @movie = {}
+  	 redirect_to "/searches" 
+  	end
   end
 end
